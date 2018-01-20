2f0aea2d65a45713485153a156d8addd50c71fdc
arothberg <Adam@adams-macbook-pro.local>
2018-01-20 16:18:14 -0500

    x

---
+++ DJILeadFollow4/DebugTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DebugTableViewCell.swift
+//  DJILeadFollow4
+//
+//  Created by Adam Rothberg on 1/20/18.
+//  Copyright © 2018 Adam Rothberg. All rights reserved.
+//
+
+import UIKit
+
+class DebugTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
